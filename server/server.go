4d8294e33ebb2ed82cb22d0d159cbee786a09172
Shuri Honda <shuri.honda065146@gmail.com>
2024-07-31 06:05:10 +0900

    add:server雛形作成

---
+++ server/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// インスタンスを作成
+	e := echo.New()
+
+	// ルートを設定
+	e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+
+	// サーバーをポート番号1323で起動
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// ハンドラーを定義
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
