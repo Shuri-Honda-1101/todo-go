6174ec1104b41ed4bae51b87d0f1af73f58cda63
Shuri Honda <shuri.honda065146@gmail.com>
2024-08-05 06:32:08 +0900

    change:マイグレーション時にschema_migrationsテーブルの有無を確認し、initialize処理を行うようにした

--- server/script/migrate.sh
+++ server/script/migrate.sh
@@ -4,9 +4,26 @@
 MIGRATION_DIR="sql/migrations"
 CONFIG_FILE="mysql.conf"
 DB_NAME="todo_go"
+INITIAL_MIGRATION_FILE="$MIGRATION_DIR/20240804_01_create_initial_tables.sql"
+
+# schema_migrations テーブルが存在するか確認
+table_exists=$(mysql --defaults-file=$CONFIG_FILE $DB_NAME -e "SHOW TABLES LIKE 'schema_migrations';" | grep -o 'schema_migrations')
+
+if [ -z "$table_exists" ]; then
+    # テーブルが存在しない場合、初期マイグレーションファイルを適用
+    if [ -f "$INITIAL_MIGRATION_FILE" ]; then
+        mysql --defaults-file=$CONFIG_FILE $DB_NAME < $INITIAL_MIGRATION_FILE
+        echo "初期設定として schema_migrations テーブルが作成されました。"
+    fi
+fi
 
 # マイグレーションファイルを順に処理
 for file in $(ls -v $MIGRATION_DIR/*.sql); do
+    # 初期マイグレーションファイルはスキップ
+    if [ "$file" == "$INITIAL_MIGRATION_FILE" ]; then
+        continue
+    fi
+
     # ファイル名をディレクトリ名を除いて取得
     filename=$(basename $file)
 
